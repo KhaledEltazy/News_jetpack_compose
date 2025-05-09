67c1bcecd301bd2a4b5b5d990b86370bdcef25f8
khaled <khmeltazy@gmail.com>
2025-05-10 01:11:09 +0300

    implementign daggerHilt

---
+++ app/src/main/java/com/android/news/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.android.news.di
+
+import android.app.Application
+import com.android.news.data.manger.LocalUserMangerImpl
+import com.android.news.domain.manger.LocalUserManger
+import com.android.news.domain.usercases.AppEntryUseCases
+import com.android.news.domain.usercases.ReadAppEntry
+import com.android.news.domain.usercases.SaveAppEntry
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideLocalUserManger(
+        application: Application
+    ) : LocalUserManger = LocalUserMangerImpl(application)
+
+    @Provides
+    @Singleton
+    fun providesAppEntryUseCases(
+        localUserManger: LocalUserManger
+    ) = AppEntryUseCases(
+        readAppEntry = ReadAppEntry(localUserManger),
+        saveAppEntry = SaveAppEntry(localUserManger)
+    )
+}
---
+++ app/src/main/java/com/android/news/domain/usercases/AppEntryUseCases.kt
@@ -0,0 +1,6 @@
+package com.android.news.domain.usercases
+
+data class AppEntryUseCases(
+    val readAppEntry: ReadAppEntry,
+    val saveAppEntry: SaveAppEntry
+)
